161092a62584148d91c44d7f8815d22feaa086b0
idunmore <62761638+idunmore@users.noreply.github.com>
2024-06-07 13:52:05 -0400

    Add common.asm include file

---
+++ common.asm
@@ -0,0 +1,13 @@
+; common.asm - Common values and macros used across files in this project for
+;              the MADS assembler.
+;
+; Copyright (C) 2024, Ian Michael Dunmore
+;
+; Licensed Under: GNU Lesser Public License v3.0
+; See: https://github.com/idunmore/atari-8bit-includes-mads/blob/main/LICENSE
+
+; Labels are case-insensitive in MADS.
+
+; True (1) and False (0) labels per MADS values for these conditions.
+TRUE = 1
+FALSE = 0
