ab3baff12ada82cf3fc49405e5567999e6903d5c
idunmore <62761638+idunmore@users.noreply.github.com>
2024-06-06 20:41:33 -0400

    Add initial macro_debugging macro

---
+++ macro_debugging.asm
@@ -0,0 +1,42 @@
+; macro_debugging.asm - Macros, etc. to assist in creating debuggable macros
+;                       and providing logging and debugging information on a
+;                       centrally controllable basis for the MADs assembler.
+;
+; Copyright (C) 2024, Ian Michael Dunmore
+;
+; Licensed Under: GNU Lesser Public License v3.0
+; See: https://github.com/idunmore/atari-8bit-includes-mads/blob/main/LICENSE
+
+; Usage:
+;
+;   .icl "macro_debugging.asm"
+;
+;   To enable macro debugging, define the label MACRO_DEBUGGING, either in
+;   source code (.define MACRO_DEBUGGING or MACRO_DEBUGGING = 1) or on the MADS
+;   command line (via -d:MACRO_DEBUGGING).
+
+; NOTE: Macros in THIS file are intended to be specific to MACRO debugging and
+; logging, hence the "Macro" prefix for macros that may have a more general
+; use (e.g. MacroDebugPrint vs. DebugPrint).  This allows different levels of
+; debugging and control to be specified independently for Macros vs.
+; general assembly code.
+
+; Macros
+
+; MacroDebugPrint - Prints the specified message if MACRO DEBUGGING is enabled.
+;
+; Avoids every macro debugging print statement having to be surrounded by:
+; .ifdef macro_debugging/.endif blocks.
+
+; Can be called with either '' or "" enclosed strings.  Using '' will result
+; in the enclosing quotes being included in the output.
+
+.macro MacroDebugPrint string
+	.ifdef macro_debugging
+		; We use the "" form of .print, as it will accept values with
+		; and without both '' or "" ("" strings are passed without
+		; enclosing quotes), since there is no way (I know of) to
+		; test partial contents of a string argument in MADS.
+		.print ":string"
+	.endif
+.endm
