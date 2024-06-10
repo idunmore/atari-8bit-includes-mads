ae851e7297afae24a62c3d1524fbf84defbe3f02
idunmore <62761638+idunmore@users.noreply.github.com>
2024-06-10 07:17:35 -0400

    Fix commentary

--- character_set.asm
+++ character_set.asm
@@ -312,7 +312,7 @@ MODE67_COLPF3 = %11000000 ; Bits %11 to select color 3
         MacroDebugPrint "Setting CHBAS to: ", :>characterSetAddress
 
         lda #>:characterSetAddress ; Get the High byte of the address
-        sta CHBAS                 ; and put it into CHBAS
+        sta CHBAS                  ; and put it into CHBAS
 .endm
 
 .endif ; _CHARACTER_SET_
