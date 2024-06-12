33eb3d39b8081c57ddce3186e24a117e443736d5
idunmore <62761638+idunmore@users.noreply.github.com>
2024-06-12 11:31:12 -0400

    Complete POKEY hardware and shadow registers

--- POKEY.asm
+++ POKEY.asm
@@ -19,6 +19,53 @@
 
 ; POKEY Hardware Registers
 
+; Audio Registers
+;
+; These values overlap with those for the paddles, but are listed first since
+; POKEY is best known for its SOUND capabilities.
+;
+; Register WRITES affect audio; READs return paddle values.
+
+AUDF1 =  $D200 ; Audio Channel 1 Frequency
+AUDF2 =  $D202 ; Audio Channel 2 Frequency
+AUDF3 =  $D204 ; Audio Channel 3 Frequency
+AUDF4 =  $D206 ; Audio Channel 4 Frequency
+
+AUDC1 =  $D201 ; Audio Channel 1 Control
+AUDC2 =  $D203 ; Audio Channel 2 Control
+AUDC3 =  $D205 ; Audio Channel 3 Control
+AUDC4 =  $D207 ; Audio Channel 4 Control
+
+AUDCTL = $D208 ; Audio Control
+
+; Paddle Potentiometer (Dial) Registers
+
+POT0 =   $D200 ; (Read) Paddle (potentiometer) 0 
+POT1 =   $D201 ; (Read) Paddle (potentiometer) 1 
+POT2 =   $D202 ; (Read) Paddle (potentiometer) 2 
+POT3 =   $D203 ; (Read) Paddle (potentiometer) 3
+POT4 =   $D204 ; (Read) Paddle (potentiometer) 4
+POT5 =   $D205 ; (Read) Paddle (potentiometer) 5
+POT6 =   $D206 ; (Read) Paddle (potentiometer) 6
+POT7 =   $D207 ; (Read) Paddle (potentiometer) 7  
+
+ALLPOT = $D208 ; (Read) Read 8 line POT port state
+POTGO =  $D20B ; Start the POT scan sequence
+
+STIMER = $D209 ; Start timers
+
+KBCODE = $D209 ; (Read) Keyboard code - This is NOT the ATASCII nor INTERNAL
+               ;        keyboard code. It is the raw code from the keyboard
+               ;        matrix (see table on page 50 of the OS Users Manual
+               ;        for ATASCII/INTERNAL translations, also keyboard.asm)
+
+RANDOM = $D20A ; (Read) Random number
+
+SKREST = $D20A ; Reset Serial Status (SKSTAT)
+SEROUT = $D20D ; Serial port data output
+SERIN =  $D20D ; (Read) Serial port data input
+SKSTAT = $D20F ; (Read) Serial port status (SKCTL write)
+
 ; SSKCTL - Serial Port Control Register - Shadowed at SSKCTL
 ;
 ; Bit   Decimal   Function
@@ -34,10 +81,14 @@
 
 SKCTL =  $D20F ; Serial Port Control
 
+; IRQEN - Interrupt Request Enable
+
+IRQEN = $D20E ; Interrupt Request Enable
+IRQST = $D20E ; (Read) IRQ Status
 
 ; POKEY Shadow Registers & Vectors
 
-POKMSK = $10 ; IRQEN - See POKEY.asm/IRQEN/IRQST for Bit-Settings
+POKMSK = $10 ; IRQEN/IRQST
 
 ; I/O Interrupt Vectors
 
@@ -55,5 +106,20 @@ VTIMR4 = $0214 ; [WORD] POKEY Timer 4 interrupt vector
 ; SSKCTL - Serial Port Control Register Shadow Register
 
 SSKCTL = $0232 ; SKCTL
+
+; Paddle Potentiometer (Dial) Shadow Registers
+
+PADDL0 = $0270 ; POT0
+PADDL1 = $0271 ; POT1
+PADDL2 = $0272 ; POT2
+PADDL3 = $0273 ; POT3
+PADDL4 = $0274 ; POT4
+PADDL5 = $0275 ; POT5
+PADDL6 = $0276 ; POT6
+PADDL7 = $0277 ; POT7
+
+; Keyboard Shadow Register
+
+CH = $02FC ; KBCODE
 
 .endif ; _POKEY_
