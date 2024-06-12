cf23c9ff8bf9b099a51419d0eef5328e53c61e10
idunmore <62761638+idunmore@users.noreply.github.com>
2024-06-12 11:48:17 -0400

    Add PIA hardware and shadow regisers

---
+++ PIA.asm
@@ -0,0 +1,51 @@
+; PIA.asm - PIA (Peripheral Interface Adapter) Registers, Values, MASKs and
+;           Bit-Settings for the MADS assembler.
+;
+; Copyright (C) 2024, Ian Michael Dunmore
+;
+; Licensed Under: GNU Lesser Public License v3.0
+; See: https://github.com/idunmore/atari-8bit-includes-mads/blob/main/LICENSE
+
+; Includes
+
+        icl 'common.asm'
+        icl 'macro_debugging.asm'
+
+; Do NOT try and apply these definitions if they are already defined.
+.ifndef _PIA_
+
+; Sentinel to allow detection of prior INCLUSION
+.def _PIA_
+
+; PIA Hardware Registers
+
+; These registers support multiple devices per register (BYTE), and require
+; decoding via various MASKs and Bit-Settings to interpret.  This decoding
+; is done during the VBI (Vertical Blank Interrupt) and the extracted,values
+; are refelected in the PIA Shadow Registers (one per device).
+
+PORTA = $D300 ; Joystick ports 1 and 2, STICK0 and STICK1
+PORTB = $D301 ; Joystick ports 3 and 4, STICK2 and STICK3
+PACTL = $D302 ; Port A Control
+PBCTL = $D303 ; Port B Control
+
+; PIA Shadow Registers
+
+STICK0 = $0278 ; Joystick 1 (decoded from PORTA)
+STICK1 = $0279 ; Joystick 2 (decoded from PORTA)
+STICK2 = $027A ; Joystick 3 (decoded from PORTB)
+STICK3 = $027B ; Joystick 4 (decoded from PORTB)
+
+; Paddle Triggers (Button) - $00 = Pressed, $01 = Not Pressed
+
+PTRIG0 = $027C ; Paddle 1 Trigger
+PTRIG1 = $027D ; Paddle 2 Trigger
+PTRIG2 = $027E ; Paddle 3 Trigger
+PTRIG3 = $027F ; Paddle 4 Trigger
+PTRIG4 = $0280 ; Paddle 5 Trigger
+PTRIG5 = $0281 ; Paddle 6 Trigger
+PTRIG6 = $0282 ; Paddle 7 Trigger
+PTRIG7 = $0283 ; Paddle 8 Trigger
+
+.endif ; _PIA_
+
