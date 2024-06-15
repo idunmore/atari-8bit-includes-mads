8d414cfa93b741080961d946c3b2ae5e5ba14a62
idunmore <62761638+idunmore@users.noreply.github.com>
2024-06-15 17:51:31 -0400

    Add tv_standard definitions for NTSC and PAL

--- common.asm
+++ common.asm
@@ -6,6 +6,10 @@
 ; Licensed Under: GNU Lesser Public License v3.0
 ; See: https://github.com/idunmore/atari-8bit-includes-mads/blob/main/LICENSE
 
+; NOTE: This file must be included BEFORE referencing any values herein, as
+;       assignments from unassigned labels, that LATER get defined, yield ZERO
+;       (rather than an error - which is possibly a MADS bug).
+
 ; Labels are case-insensitive in MADS.
 
 ; Do NOT try and apply these definitions if they are already defined.
@@ -15,15 +19,22 @@
 .def _COMMON_
 
 ; True (1) and False (0) labels per MADS values for these conditions.
+
 TRUE = 1
 FALSE = 0
 
 ; Common Atari alignment/boundaries.
+
 PAGE_BOUNDARY = $100
 BOUNDARY_1K   = $400
 BOUNDARY_2K   = $800
 
 CHBASE_BOUNDARY = BOUNDARY_1K ; 1K boundary for Character Sets (CHBASE)
 PMBASE_BOUNDARY = BOUNDARY_2K ; 2K boundary for Player/Missile Graphics (PMBASE)
+
+; TV/Color Standards (for "tv_standard" label)
+
+NTSC = $00
+PAL =  $01
 
 .endif ; _COMMON_
